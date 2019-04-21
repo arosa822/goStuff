c47928ff551c071a82f819931be532945797dc57
arosa822 <h.rosa822@gmail.com>
2019-04-21 19:38:07 -0400

    add more examples to variables

--- constants/variables.go
+++ constants/variables.go
@@ -9,11 +9,24 @@ const (
     Small = Big >> 61
 )
 
+var pi float32  = 3223.14
+
 func main(){
-    const Greetings = "hello" 
-    fmt.Println(Pi)
-    fmt.Println(Truth)
-    fmt.Println(Big)
-    fmt.Println(Small)
-    fmt.Println(Greetings)
+    const Greetings = "hello"
+    //fmt.Println(Pi)
+    //fmt.Println(Truth)
+    //fmt.Println(Big)
+    //fmt.Println(Small)
+    //fmt.Println(Greetings+ "\n")
+    fmt.Printf("%s \n big = %d \n small = %d\n", Greetings, Big, Small)
+
+    cycloneModel := 6
+    fmt.Println(cycloneModel)
+    name := "caprica-six"
+    aka := fmt.Sprintf("Number %d",6)
+    fmt.Printf("%s is also a known as %s\n",
+        name, aka)
+    //pi = pi + 2
+    pi = Pi + 1
+    fmt.Println(pi)
 }
