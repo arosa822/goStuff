a7b92ba919910a7100d341b2839da97e9ef6a517
arosa822 <h.rosa822@gmail.com>
2019-04-28 19:35:27 -0600

    finish 5.5 example

---
+++ cp_class/crawler/findlinks1/findlinks1.go
@@ -0,0 +1,42 @@
+// FindLinks1 prints the links in an HTML document read from standard input.
+package main
+
+import (
+    "fmt"
+    "os"
+    "golang.org/x/net/html"
+)
+
+func main() {
+    // assign doc to os.Stdin via html
+    doc, err := html.Parse(os.Stdin)
+    // error check
+    if err != nil {
+        fmt.Fprintf(os.Stderr," findlink1: %v\n", err)
+        os.Exit(1)
+    }
+    for _, link :=range visit(nil, doc) {
+        fmt.Println(link)
+    }
+}
+
+/*
+ The visit function below traverses an HTML node tree, extracts the link from the 
+ href attribute ofeach anchor element <a href='...'> appends the links to a slice 
+ of strings and returns the resulting slice
+*/
+
+// visit appends the links each link found in n and returns the results
+func visit(links []string, n *html.Node) []string {
+    if n.Type == html.ElementNode && n.Data == "a" {
+        for _, a := range n.Attr {
+            if a.Key == "href" {
+                links = append(links, a.Val)
+            }
+        }
+    }
+    for c := n.FirstChild; c != nil; c = c.NextSibling {
+        links = visit(links,c)
+    }
+    return links
+}
