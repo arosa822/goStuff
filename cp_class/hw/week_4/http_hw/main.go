e95572987fa98cec34697f243c02feafd044139c
arosa822 <h.rosa822@gmail.com>
2019-05-13 18:19:35 -0600

    work on http examples and hw

---
+++ cp_class/hw/week_4/http_hw/main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"strconv"
+)
+
+type dollars float32
+
+func (d dollars) String() string { return fmt.Sprintf("$%.2f", d) }
+
+func main() {
+	db := database{"shoes": 50, "socks": 5}
+	http.HandleFunc("/list", db.list)
+	http.HandleFunc("/price", db.price)
+	http.HandleFunc("/update", db.update)
+	log.Fatal(http.ListenAndServe("localhost:8000", nil))
+}
+
+type database map[string]dollars
+
+func (db database) list(w http.ResponseWriter, req *http.Request) {
+	for item, price := range db {
+		fmt.Fprintf(w, "%s: %s\n", item, price)
+	}
+}
+
+func (db database) price(w http.ResponseWriter, req *http.Request) {
+	item := req.URL.Query().Get("item")
+	price, ok := db[item]
+	if !ok {
+		w.WriteHeader(http.StatusNotFound) //404
+		fmt.Fprintf(w, "no such item: %q\n", item)
+		return
+	}
+	fmt.Fprintf(w, "%s\n", price)
+}
+
+func (db database) update(w http.ResponseWriter, req *http.Request) {
+	item := req.URL.Query().Get("item")
+	price := req.URL.Query().Get("price")
+	if _, ok := db[item]; ok {
+		f, err := strconv.ParseFloat(price, 64)
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest) // 404
+			fmt.Fprintf(w, "cannot parse price: %v\n", err)
+		}
+		if f > 0 {
+			db[item] = dollars(f)
+		} else {
+			w.WriteHeader(http.StatusBadRequest) //404
+			fmt.Fprintf(w, "price must be > 0: %f\n", f)
+		}
+	}
+}
